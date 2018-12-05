e5610bb8f2fdaebb050a4e7017d980c82be11790
Erasmo Leon Lopez <erasmo.leon@globant.com>
2018-12-05 15:00:26 -0600

    validations for commands

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import TelegramBot from 'node-telegram-bot-api';
 const token = '765371044:AAFEKO41gu7BPi3GdVMeswbsqjAQvzS0ExA';
 const bot = new TelegramBot(token, { polling: true });
 
-const whiteList = [677959493];
+const myId = 677959493;
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+const isBotCommand = message =>
+  message.entities && message.entities[0].type === 'bot_command';
+
+bot.on('message', (message) => {
+  const chatId = message.chat.id;
+  if (message.from.id != myId) {
+    return bot.sendMessage(chatId, 'No eres un usuario valido!!');
+  }
+  if (!isBotCommand(message)) {
+    return bot.sendMessage(chatId, 'Escribe un comando valido');
+  }
   bot.sendMessage(chatId, 'Received your message');
 });
